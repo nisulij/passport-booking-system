1fad475d18d707c4d492905dfd1b70f157c010c0
nisulij <nisulijayasuriya@gmail.com>
2026-05-17 00:13:05 +0200

    fix cors for deployed frontend

--- server/server.js
+++ server/server.js
@@ -1,50 +1,118 @@
 require("dotenv").config();
 
-console.log("EMAIL:", process.env.EMAIL || "missing");
 console.log(
-  "EMAIL_PASSWORD:",
-  process.env.EMAIL_PASSWORD ? "loaded" : "missing"
+"EMAIL:",
+process.env.EMAIL || "missing"
 );
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+console.log(
+"EMAIL_PASS:",
+process.env.EMAIL_PASS ? "loaded" : "missing"
+);
 
-const bookingRoutes = require("./routes/booking");
-const adminRoutes = require("./routes/admin");
+const express=require("express");
+const mongoose=require("mongoose");
+const cors=require("cors");
 
-const app = express();
+const bookingRoutes=
+require("./routes/booking");
+
+const adminRoutes=
+require("./routes/admin");
+
+const app=express();
+
+
+// CORS FIX
 
 app.use(
-  cors({
-    origin: [
-      "passport-booking-system-git-main-nisuli-s-projects.vercel.app",
-    ],
-    credentials: true
-  })
+
+cors({
+
+origin:true,
+
+credentials:true,
+
+methods:[
+"GET",
+"POST",
+"PUT",
+"DELETE"
+],
+
+allowedHeaders:[
+"Content-Type",
+"Authorization"
+]
+
+})
+
 );
 
-app.use(express.json());
 
-app.use("/api", bookingRoutes);
+app.use(
+express.json()
+);
 
-app.use("/api/admin", adminRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Passport API running");
-});
+app.use(
+"/api",
+bookingRoutes
+);
+
+app.use(
+"/api/admin",
+adminRoutes
+);
+
+
+app.get(
+"/",
+(req,res)=>{
+
+res.send(
+"Passport API running"
+);
+
+}
+);
+
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch((err) => {
-    console.log("Mongo Error:", err);
-  });
+.connect(
+process.env.MONGO_URL
+)
 
-const PORT = process.env.PORT || 5000;
+.then(()=>{
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+console.log(
+"MongoDB Connected"
+);
+
+})
+
+.catch((err)=>{
+
+console.log(
+"Mongo Error:",
+err
+);
+
+});
+
+
+const PORT=
+process.env.PORT || 5000;
+
+
+app.listen(
+
+PORT,
+
+()=>{
+
+console.log(
+`Server running on port ${PORT}`
+);
+
 });
